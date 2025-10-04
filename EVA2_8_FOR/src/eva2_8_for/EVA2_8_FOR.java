7f2bbc6d3bc029da2a6c614c236a913472dbeccb
Richy23-oss <riardo.bencomz@gmail.com>
2025-10-03 23:53:45 -0600

    Add files via upload

---
+++ EVA2_8_FOR/src/eva2_8_for/EVA2_8_FOR.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_8_for;
+
+/**
+ *
+ * @author rosal
+ */
+public class EVA2_8_FOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //3 parametros 
+        //variable y valor de inicio
+        //hasta donde llegas CONDICION --- TIENE QUE SER VERDADERA
+        //CUANDO ES FASLO EL FOR SE DETIENE
+        //incremento o decremento
+        //SUMAR O RESTAR
+        //OPERADORES: AÑADIR 1 O RESTAR 
+        //++ --
+        //i++ SIGNIFICA SUMARLE 1 AL VALOR DE LA i
+        for(int i =1; i <= 10; i++){
+           System.out.println("HOLA MUJNDO!!");
+   }
+         for(int i = 10; i>=1;1--){
+         System.out.println(i);
+}
+
