fd2551197dc0a801bdd01aec765d8d3bb3a2092b
Richy23-oss <riardo.bencomz@gmail.com>
2025-10-15 22:34:35 -0700

    Add files via upload

--- Eva2_14_CAPTURA/src/main/java/com/mycompany/eva2_14_captura/Eva2_14_CAPTURA.java
+++ Eva2_14_CAPTURA/src/main/java/com/mycompany/eva2_14_captura/Eva2_14_CAPTURA.java
@@ -13,20 +13,28 @@ import java.util.Scanner;
 public class Eva2_14_CAPTURA {
 
     public static void main(String[] args) {
-       Scanner captu = new Scanner (System.in);
-       int tama; 
-       System.out.println("cuantas calificaciones necesitas:");
-       tama = captu.nextInt();
-       
-       int califas [] = new int [tama];
-       //capturar 
-       for (int i =0; i < califas.length; i++){
-           System.out.println("calificacion: " + (i+1) + ":");
-           califas [i]= captu.nextInt();
-           
-       }
-       for (int i = 0; i < califas.length; i++){
-           System.out.println("[" + califas [i] + "]");
+        Scanner captu = new Scanner (System.in);
+        int tama;
+        //capturar
+        System.out.println("cauntas calificacionjes va a capturar?");
+        tama=captu.nextInt();
+        int califas[] = new int [tama];
+        for(int i = 0; i< califas.length; i++){
+            System.out.println("calificacion" + (i+1) + ":");
+            califas[i]= captu.nextInt();
+        }
+        //leer
+        for(int i = 0; i< califas.length; i++){
+            System.out.println("[" + califas[i] + "]");
+        }
+        int suma = 0;
+        //SUMAR TODAS LAS CALIFICACIONES
+        for(int i = 0; i< califas.length; i++){
+            suma = suma + califas[i];
+            //suma += califas[i]
+        }
+        double promedio + suma / tama; 
+        System.out.println("promedio = "+ promedio);
+        
     }
-    }   
 }
---
+++ Eva2_15_ACUMULADORES/src/main/java/com/mycompany/eva2_15_acumuladores/Eva2_15_ACUMULADORES.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_15_acumuladores;
+
+/**
+ *
+ * @author riard
+ */
+public class Eva2_15_ACUMULADORES {
+
+    public static void main(String[] args) {
+     int cuenta = 0;
+     //ASIGNACION
+     System.out.println(cuenta);
+     cuenta=cuenta+100; //ACUMULADORES
+     System.out.println(cuenta);
+     cuenta=cuenta+1000;
+     System.out.println(cuenta);
+     cuenta=cuenta- 800; //CUENTA -= 800;
+     cuenta+=2000;
+     System.out.println(cuenta); //CUENTA = CUENTA + 2000
+    }
+}
